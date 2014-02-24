d622e2e189d64b1bd9b98ba232ba7bfd1360a9f9
hancong03 <121371790@qq.com>
2014-02-24 18:13:22 +0800

    公式优化
    Signed-off-by: hancong03 <121371790@qq.com>

--- src/char/data.js
+++ src/char/data.js
@@ -77,20 +77,22 @@ define( function ( require, exports, module ) {
             Z: require( "char/data/character/uz" ),
 
             // symbol
-            "(": require( "char/data/symbol/lparentheses" ),
-            ")": require( "char/data/symbol/rparentheses" ),
+            "(": require( "char/data/symbol/l-parentheses" ),
+            ")": require( "char/data/symbol/r-parentheses" ),
             "-": require( "char/data/symbol/negative" ),
             "+": require( "char/data/symbol/positive" ),
             "|": require( "char/data/symbol/vertical" ),
             "/": require( "char/data/symbol/slash" ),
             "!": require( "char/data/symbol/exclamation" ),
-            "[": require( "char/data/symbol/lbrackets" ),
-            "]": require( "char/data/symbol/rbrackets" ),
+            "[": require( "char/data/symbol/l-brackets" ),
+            "]": require( "char/data/symbol/r-brackets" ),
             ":": require( "char/data/symbol/colon" ),
             "'": require( "char/data/symbol/quotation" ),
             "<": require( "char/data/symbol/lt" ),
             ">": require( "char/data/symbol/gt" ),
             ".": require( "char/data/symbol/point" ),
+            "{": require( "char/data/symbol/l-braces" ),
+            "}": require( "char/data/symbol/r-braces" ),
 
             // greek
             "\u03B1": require( "char/data/greek/alpha" ),
