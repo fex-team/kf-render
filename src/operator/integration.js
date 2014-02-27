6ebc7f34663d1461fe61051ab1824faa731e0204
hancong03 <121371790@qq.com>
2014-02-27 13:16:32 +0800

    积分函数升级
    Signed-off-by: hancong03 <121371790@qq.com>

--- src/operator/integration.js
+++ src/operator/integration.js
@@ -66,87 +66,128 @@ define( function ( require, exports, modules ) {
 
     }
 
-    function adjustmentPosition ( operatorShape, integrand, supOperand, subOperand ) {
+    function adjustmentPosition ( operatorShape, integrandExp, supOperand, subOperand ) {
 
-        var operatorBox = operatorShape.getRenderBox(),
-            integrandBox = integrand.getRenderBox(),
-            supBox = null,
-            subBox = null,
-            expBox = null,
-            // 被积函数的偏移
+//        var operatorBox = operatorShape.getRenderBox(),
+//            integrandBox = integrand.getRenderBox(),
+//            supBox = null,
+//            subBox = null,
+//            expBox = null,
+//            // 被积函数的偏移
+//            offset = {
+//                x: operatorBox.width,
+//                y: 0
+//            },
+//            // 表达式左边部分最大宽度
+//            leftMaxWidth = 0,
+//            maxHeight = 0;
+//
+//        if ( supOperand ) {
+//
+//            supOperand.setAnchor( 0, 0 ).scale( 0.7 );
+//            subOperand.setAnchor( 0, 0 ).scale( 0.7 );
+//
+//            supBox = supOperand.getRenderBox();
+//            subBox = subOperand.getRenderBox();
+//
+//            // 上限偏移
+//            offset.x += 2;
+//            supOperand.translate( offset.x, 2 );
+//
+//            leftMaxWidth = offset.x + supBox.width;
+//
+//            // 符号偏移
+//            offset.y += supBox.height / 2;
+//            offset.x += supBox.width;
+//            operatorShape.translate( 0, offset.y );
+//
+//            // 下限偏移
+//            subOperand.translate( operatorBox.width - 8, offset.y + operatorBox.height - subBox.height + 6 );
+//
+//            leftMaxWidth = Math.max( leftMaxWidth, operatorBox.width - 8 + subBox.width );
+//
+//        }
+//
+//        /* 被积函数和操作符及上下限的偏移 */
+//
+//        // 被积函数偏移
+//        maxHeight = Math.max( operatorBox.height, integrandBox.height );
+//        integrand.translate( leftMaxWidth + 2, ( maxHeight - integrandBox.height ) / 2 + offset.y - integrandBox.y );
+//
+//        // 操作符偏移
+//        maxHeight = ( maxHeight - operatorBox.height ) / 2;
+//        operatorShape.translate( 0, maxHeight );
+//        supOperand.translate( 0, maxHeight );
+//        subOperand.translate( 0, maxHeight );
+//
+//        /* 操作符边框的调整， 包裹住所有内容 */
+//        // 获取整个表达式经过调整后的大小
+//        expBox = this.parentExpression.getRenderBox();
+//
+//        // 要保证符号上下的空间是相同的
+//        var diff = expBox.height - offset.y - operatorBox.height;
+//
+//        this.box.setWidth( expBox.width );
+//
+//        // 下部空间大于上部空间， 调整整个空间
+//        if ( diff > offset.y ) {
+//
+//            diff = diff - offset.y;
+//
+//            // 移动整个空间中的内容
+//            this.box.setHeight( expBox.height + diff );
+//            supOperand.translate( 0, diff );
+//            subOperand.translate( 0, diff );
+//            operatorShape.translate( 0, diff );
+//            integrand.translate( 0, diff );
+//
+//        } else {
+//        // 仅调整边框大小
+//
+//            this.box.setHeight( expBox.height + offset.y - diff );
+//
+//        }
+        supOperand.setAnchor( 0, 0 ).scale( 0.7 );
+        subOperand.setAnchor( 0, 0 ).scale( 0.7 );
+
+        var expBox = integrandExp.getRenderBox(),
+            subBox = subOperand.getRenderBox(),
+            supBox = supOperand.getRenderBox(),
+            opBox = operatorShape.getRenderBox(),
+            maxScriptSize = {
+                width: Math.max( subBox.width, supBox.width ),
+                height: Math.max( subBox.height, supBox.height )
+            },
+            maxLeftWidth = Math.max( opBox.width, maxScriptSize.width ),
             offset = {
-                x: operatorBox.width,
+                x: 0,
                 y: 0
             },
-            // 表达式左边部分最大宽度
-            leftMaxWidth = 0,
-            maxHeight = 0;
+            // 左右两部分的高度diff
+            leftHeight = subBox.height / 3 + supBox.height / 3 + opBox.height,
+            // 表达式在Y轴上的偏移
+            expOffsetY = 0;
 
-        if ( supOperand ) {
-
-            supOperand.setAnchor( 0, 0 ).scale( 0.7 );
-            subOperand.setAnchor( 0, 0 ).scale( 0.7 );
-
-            supBox = supOperand.getRenderBox();
-            subBox = subOperand.getRenderBox();
-
-            // 上限偏移
-            offset.x += 2;
-            supOperand.translate( offset.x, 2 );
-
-            leftMaxWidth = offset.x + supBox.width;
-
-            // 符号偏移
-            offset.y += supBox.height / 2;
-            offset.x += supBox.width;
-            operatorShape.translate( 0, offset.y );
-
-            // 下限偏移
-            subOperand.translate( operatorBox.width - 8, offset.y + operatorBox.height - subBox.height + 6 );
-
-            leftMaxWidth = Math.max( leftMaxWidth, operatorBox.width - 8 + subBox.width );
-
-        }
-
-        /* 被积函数和操作符及上下限的偏移 */
-
-        // 被积函数偏移
-        maxHeight = Math.max( operatorBox.height, integrandBox.height );
-        integrand.translate( leftMaxWidth + 2, ( maxHeight - integrandBox.height ) / 2 + offset.y - integrandBox.y );
-
-        // 操作符偏移
-        maxHeight = ( maxHeight - operatorBox.height ) / 2;
-        operatorShape.translate( 0, maxHeight );
-        supOperand.translate( 0, maxHeight );
-        subOperand.translate( 0, maxHeight );
-
-        /* 操作符边框的调整， 包裹住所有内容 */
-        // 获取整个表达式经过调整后的大小
-        expBox = this.parentExpression.getRenderBox();
-
-        // 要保证符号上下的空间是相同的
-        var diff = expBox.height - offset.y - operatorBox.height;
-
-        this.box.setWidth( expBox.width );
-
-        // 下部空间大于上部空间， 调整整个空间
-        if ( diff > offset.y ) {
-
-            diff = diff - offset.y;
-
-            // 移动整个空间中的内容
-            this.box.setHeight( expBox.height + diff );
-            supOperand.translate( 0, diff );
-            subOperand.translate( 0, diff );
-            operatorShape.translate( 0, diff );
-            integrand.translate( 0, diff );
-
+        // 调整左右两边的高度, 使之不会出现负偏移
+        if ( leftHeight < expBox.height ) {
+            offset.y = ( expBox.height - leftHeight ) / 2;
         } else {
-        // 仅调整边框大小
-
-            this.box.setHeight( expBox.height + offset.y - diff );
-
+            expOffsetY = ( leftHeight - expBox.height ) / 2;
         }
+
+        offset.x = opBox.width + 1;
+        supOperand.translate( offset.x, offset.y );
+        offset.y += supBox.height / 3;
+
+        operatorShape.translate( 0, offset.y );
+        offset.y += opBox.height - subBox.height * 2 / 3;
+
+        subOperand.translate( opBox.width - 9, offset.y );
+
+        // 左边部分所占空间大小
+        offset.x = Math.max( opBox.width + 1 + supBox.width, opBox.width + subBox.width - 9 );
+
+        integrandExp.translate( offset.x + 5, expOffsetY );
 
     }
 
